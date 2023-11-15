a66901f2b09998412d929a74665bb26eca2a9baf
unknown <86117774+ThisIsMyFirstTime@users.noreply.github.com>
2023-11-14 22:28:54 -0600

    updated main.c with guessing game program incomplete

--- main.c
+++ main.c
@@ -1,32 +1,47 @@
 #include <stdio.h>
-
+#include <stdlib.h>
+#include <time.h>
 
 int main()
 {
+    time_t t;
+
+    srand((unsigned) time(&t));
+
     int guesses = 0;
     int user_guess;
-    int random_number = 7;
+    int random_number = rand() % 21;
     int max_guesses = 5;
 
-    while (guesses <= max_guesses)
+    while (guesses < max_guesses)
     {
-        printf("Guess the random number: ");
-        scanf("%d", user_guess);
+        printf("\nGuess the random number between 0 and 20: ");
+        scanf("%d", &user_guess);
 
-        printf("\n\nYou guessed %d.\n\n", user_guess);
+        printf("\nYou guessed %d.\n", user_guess);
 
         if (user_guess == random_number)
         {
-            printf("\n\nCongratulations you guessed the correct number!\n\n");
+            printf("\nCongratulations you guessed the correct number!\n\n");
             return 0;
+        }
+
+        if (user_guess < random_number)
+        {
+            printf("\nYou guessed too low!\n");
+        }
+        
+        if (user_guess > random_number)
+        {
+            printf("\nYou guessed too high!\n");
         }
 
         ++guesses;
 
-        printf("That was incorrect, you have %d attempts left.", max_guesses - guesses);
+        printf("\nYou have %d attempts left.\n\n", max_guesses - guesses);
     }
 
-    printf("You have ran out of guesses! Play again!");
+    printf("\nYou have ran out of guesses! Play again!\n\n");
 
     return 0;
 }
