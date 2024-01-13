e94312f7a9e70086cca3971e26c69605427df756
Tristan <86117774+ThisIsMyFirstTime@users.noreply.github.com>
2024-01-13 13:17:52 -0600

    starting string io functions

--- advanced/src/char_funcs_challenge.c
+++ advanced/src/char_funcs_challenge.c
@@ -7,3 +7,32 @@ Char input and output functions Challenge
     - if there is no argument, it is interpreted as the name of a file 
     - if there is no argument, the standard input(stdin) is to be used for input
 */
+#include <stdio.h>
+
+int main(void)
+{
+    FILE *fp = NULL;
+    int ch = '\0', count = 0;
+    
+    fp = fopen("./notes/c99.txt", "r");
+
+    if(!fp)
+    {
+        printf("The file is unable to be opened.\n");
+
+        return 1;
+    }
+    while((ch = fgetc(fp)) != EOF)
+        {
+            if(ch <= 90 && ch >= 65)
+                count++;
+            if(ch <= 122 && ch >= 97)
+                count++;
+        }
+
+    printf("The number of characters in this file are: %d\n", count);
+
+    fclose(fp);
+
+    return 0;
+}
