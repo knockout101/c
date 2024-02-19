f45a795ba75a98693aa11f31fcfdc437c56d97bf
unknown <86117774+ThisIsMyFirstTime@users.noreply.github.com>
2024-02-19 15:16:39 -0600

    dlfcn.h need to switch to mac

--- advanced/lib/src/string_funcs.c
+++ advanced/lib/src/string_funcs.c
@@ -13,8 +13,6 @@ should contain implementations of various string manipulation functions
 #include <string.h>
 #include <ctype.h>
 
-#include "strfuncs.h"
-
 int count( char* string, char ch)
 {
     int count = 0, i = 0;
