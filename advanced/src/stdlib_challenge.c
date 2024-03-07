690c79cd70f1455d420c5a53c19df0bab314b520
unknown <86117774+ThisIsMyFirstTime@users.noreply.github.com>
2024-03-07 08:37:29 -0600

    preliminary code written, needs final checks

--- advanced/src/stdlib_challenge.c
+++ advanced/src/stdlib_challenge.c
@@ -38,7 +38,7 @@ Challenge #4
 
 - write a program to compute the number of seconds passed since the beginning of the current month 
     - you should use the localtime, difftime, and mktime functions 
-    - you should handle errors uding fprintf and the exit function with the correct failure codes 
+    - you should handle errors using fprintf and the exit function with the correct failure codes 
 */
 #include <stdio.h>
 #include <time.h>
@@ -51,15 +51,21 @@ void challenge3(void);
 void challenge4(void);
 void fillArray(double* ar, int size);
 void showArray(double* ar, int size);
+int sortDoubles(const void* a, const void* b);
 
-#define max(x, y) (((x) >= (y)) ? (x) : (y))
 
 #define MAX_DOUBLE 50
 
 int main(void)
 {
+    printf("\nChallenge1:\n\n");
     challenge1();
+    printf("\nChallenge2:\n\n");
     challenge2();
+    printf("\nChallenge3:\n\n");
+    challenge3();
+    printf("\nChallenge4:\n\n");
+    challenge4();
 
     return 0;
 }
@@ -90,25 +96,33 @@ void challenge2(void)
 {
     double ar[MAX_DOUBLE];
 
-    
-
     fillArray(ar, MAX_DOUBLE);
 
+    printf("Random Array\n");
     showArray(ar, MAX_DOUBLE);
 
+    qsort(ar, MAX_DOUBLE, sizeof(double), sortDoubles);
+
+    printf("\nSorted array:\n");
+    showArray(ar, MAX_DOUBLE);
 }
 
-double sortDoubles(const void* a, const void* b)
+int sortDoubles(const void* a, const void* b)
 {
-    return max(*(double*)a, *(double*)b);
+    const double *A = a, *B = b;
+    if(*A > *B)
+        return 1;
+    else if(*A < *B)
+        return -1;
+    else
+        return 0;
 }
 
 void fillArray(double* ar, int size)
 {
     for(int i = 0; i < size; i++)
-    {
         ar[i] = ((double)rand()/(double)RAND_MAX) * 40;
-    }
+    
 }
 
 void showArray(double* ar, int size)
@@ -126,4 +140,34 @@ void showArray(double* ar, int size)
             count = 0;
         }
     }
+    putchar('\n');
+}
+
+void challenge3(void)
+{
+    time_t calendar;
+
+    time(&calendar);
+
+    printf("Today's date is: %s", ctime(&calendar));
+}
+
+void challenge4(void)
+{
+   time_t current_t, prev_mon, ret;
+
+    time(&current_t);
+    time(&prev_mon);
+
+    struct tm* prev_struct = localtime(&current_t);
+
+    prev_struct->tm_mday = 1;
+
+    if((ret = mktime(prev_struct)) == -1)
+    {
+        fprintf(stderr, "Operation failed.");
+        exit(EXIT_FAILURE);
+    }
+
+    printf("The seconds passed sense the beginning of the month: %fs", difftime(current_t, ret));
 }
