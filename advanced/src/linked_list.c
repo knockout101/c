b979ab552ff5c9934dcfb962d530154b065dd0b1
unknown <86117774+ThisIsMyFirstTime@users.noreply.github.com>
2024-03-12 21:05:23 -0500

    linked list challenge

--- advanced/src/linked_list.c
+++ advanced/src/linked_list.c
@@ -79,7 +79,7 @@ int main(void)
                 else
                     printf("List is empty.\n\n");
                 break;
-                
+
             case 5:
                 if(!isEmpty(head))
                 {
@@ -96,3 +96,144 @@ int main(void)
 
     return 0;
 }
+
+void insertAtBeginning(ListNodePtr *head, char val)
+{
+    ListNodePtr new_node = malloc(sizeof(node_t));
+    new_node->data = val;
+    new_node->nextPtr = *head;
+    *head = new_node;
+}
+
+void insertAtEnd(ListNodePtr *head, char val)
+{
+    ListNodePtr current = *head;
+
+    if(current != NULL)
+    {
+        while(current->nextPtr != NULL)
+            current = current->nextPtr;
+    
+
+        current->nextPtr = malloc(sizeof(node_t));
+        current->nextPtr->data = val;
+        current->nextPtr->nextPtr = NULL;
+    }
+    else
+    {
+        current = malloc(sizeof(node_t));
+        current->data = val;
+        current->nextPtr = NULL;
+        *head = current;
+    }
+}
+
+void insert(ListNodePtr *head, char value)
+{
+    ListNodePtr newPtr;
+    ListNodePtr previousPtr;
+    ListNodePtr currentPtr;
+
+    newPtr = malloc(sizeof(node_t));
+
+    if(newPtr != NULL)
+    {
+        newPtr->data = value;
+        newPtr->nextPtr = NULL;
+
+        previousPtr = NULL;
+        currentPtr = *head;
+
+        while(currentPtr != NULL && value > currentPtr->data)
+        {
+            previousPtr = currentPtr;
+            currentPtr = currentPtr->nextPtr;
+        }
+
+        if(previousPtr == NULL)
+        {
+            newPtr->nextPtr = *head;
+            *head = newPtr;
+        }
+        else
+        {
+            previousPtr->nextPtr = newPtr;
+            newPtr->nextPtr = currentPtr;          
+        }
+    }
+    else
+        printf("%c is not inserted. No memory available\n", value);
+}
+
+void deleteAtBeginning(ListNodePtr *head)
+{
+    ListNodePtr tempPtr = NULL;
+
+    if(head == NULL)
+        return;
+    else
+    {
+        tempPtr = *head;
+        *head = (*head)->nextPtr;
+        free(tempPtr);
+    }
+}
+
+ char delete(ListNodePtr *head, char value)
+ {
+    ListNodePtr previousPtr;
+    ListNodePtr currentPtr;
+    ListNodePtr tempPtr;
+
+    if (value == (*head)->data)
+    {
+        tempPtr = *head;
+        *head = (*head)->nextPtr;
+        free(tempPtr);
+        return value;
+    }
+    else
+    {
+        previousPtr = *head;
+        currentPtr = (*head)->nextPtr;
+
+        while(currentPtr != NULL && currentPtr->data != value)
+        {
+            previousPtr = currentPtr;
+            currentPtr = currentPtr->nextPtr;
+        }
+
+        if(currentPtr != NULL)
+        {
+            tempPtr = currentPtr;
+            previousPtr->nextPtr = currentPtr->nextPtr;
+            free(tempPtr);
+            return value;
+        }
+    }
+    
+    return '\0';
+ }
+
+ void printList(ListNodePtr currentPtr)
+ {
+    if(currentPtr == NULL)
+    {
+        printf("List is empty.\n\n");
+    }
+    else
+        printf("The list is:\n");
+
+        while(currentPtr != NULL)
+        {
+            printf("%c -->", currentPtr->data);
+            currentPtr = currentPtr->nextPtr;
+        }
+
+        printf("NULL\n\n");
+ }
+
+ int isEmpty(ListNodePtr head)
+ {
+    return head == NULL;
+ }
---
+++ advanced/src/linked_list_challenge.c
@@ -0,0 +1,210 @@
+/*
+Linked List Challenge
+
+- write a program that performs operations on a linked list 
+
+- you need to create a linked list that stores integers and uses pointers 
+
+- your program should perform the following operations
+    - insert node at first 
+    - insert node at last 
+    - insert node at position
+    - delete node from any position
+    - Update node value 
+    - search element in the linked list 
+    - display list 
+    - exit
+*/
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct node
+{
+    int data;
+    struct node* nextPtr;
+} node_t;
+
+typedef node_t* ListNodePtr;
+
+void insertAtBeginning(ListNodePtr* head, int value);
+void insertAtEnd(ListNodePtr* head, int value);
+void insert(ListNodePtr* head, int value);
+int delete(ListNodePtr* head, int position, int value);
+void update(node_t* node, int updated_value);
+node_t findFirst(ListNodePtr* head, int value);
+
+void printList(ListNodePtr head);
+
+int main(void)
+{
+    ListNodePtr head = NULL;
+    int choice = 0, select = 0, select2 = 0;
+
+    printf("Please enter a number to choose from the list:\n"
+        "   1 - insert node at beginning\n"
+        "   2 - insert node at the end\n"
+        "   3 - insert node into specific position\n"
+        "   4 - delete node from any position\n"
+        "   5 - update node value\n"
+        "   6 - find first element in the linked list\n"
+        "   7 - display list\n"
+        "   8 - exit program\n");
+
+    printf(">> ");
+    scanf("%d", &choice);
+
+    while(choice != 8)
+    {
+        switch(choice)
+        {
+            case 1:
+                printf("Enter a number value to be stored in the node: ");
+                scanf("%d", &select);
+                insertAtBeginning(&head, select);
+                printList(head);
+                break;
+            case 2:
+                printf("Enter a number value to be stored in the node: ");
+                scanf("%d", &select);
+                insertAtEnd(&head, select);
+                printList(head);
+                break;
+            case 3:
+                printf("Enter a number value to be stored in the node: ");
+                scanf("%d", &select);
+                insert(&head, select);
+                printList(head);
+                break;
+            case 4:
+                printf("Enter a position to delete the node from: ");
+                scanf("%d", &select);
+                delete(&head, select);
+                printList(head);
+                break;
+            case 5:
+                printf("enter a position followed by a whole number to update that position (x y): ");
+                scanf("%d %d", &select, &select2);
+                update(&head, select, select2);
+                printList(head);
+                break;
+            case 6:
+                printf("Enter a value you'd like to find in the linked list (returns first find): ");
+                scanf("%d", select);
+                findFirst(&head, select);
+                printList(head);
+                break;
+            case 7:
+                printList(head);
+                break;
+        }
+    }
+    
+    return 0;
+}
+
+void insertAtBeginning(ListNodePtr* head, int value)
+{
+    ListNodePtr new_node = malloc(sizeof(node_t));
+    new_node->data = value;
+    new_node->nextPtr = *head;
+    *head = new_node;
+}
+
+void insertAtEnd(ListNodePtr* head, int value)
+{
+    ListNodePtr current = *head;
+
+    if(current != NULL)
+    {
+        while(current->nextPtr != NULL)
+            current = current->nextPtr;
+        
+        current->nextPtr = malloc(sizeof(node_t));
+        current->nextPtr->data = value;
+        current->nextPtr->nextPtr = NULL;
+    }
+    else
+    {
+        current->nextPtr = malloc(sizeof(node_t));
+        current->nextPtr->data = value;
+        current->nextPtr->nextPtr = NULL;
+        *head = current;
+    }
+}
+
+void insert(ListNodePtr* head, int value)
+{
+    ListNodePtr currentPtr, prevPtr, newPtr;
+
+    newPtr = malloc(sizeof(node_t));
+
+    if(newPtr != NULL)
+    {
+        newPtr->data = value;
+        newPtr->nextPtr = NULL;
+
+        prevPtr = NULL;
+        currentPtr = *head;
+
+        while(currentPtr->nextPtr != NULL && value > currentPtr->data)
+        {
+            prevPtr = currentPtr;
+            currentPtr = currentPtr->nextPtr;
+        }
+        if(prevPtr == NULL)
+        {
+            newPtr->nextPtr = *head;
+            *head = newPtr;
+        }
+        else
+        {
+            prevPtr->nextPtr = newPtr;
+            newPtr->nextPtr = currentPtr;
+        }
+    }
+    else
+        printf("The memory was not able to be allocated for %d", value);
+}
+
+int delete(ListNodePtr* head, int value)
+{
+    ListNodePtr prevPtr, currentPtr, tempPtr;
+    
+    if(value == (*head)->data)
+    {
+        tempPtr = *head;
+        *head = (*head)->nextPtr;
+        free(tempPtr);
+        return value;
+    }
+    else 
+    {
+        currentPtr = *head;
+        prevPtr = NULL;
+        
+        while(currentPtr->nextPtr != NULL && currentPtr->data != value)
+        {
+            prevPtr = currentPtr;
+            currentPtr = currentPtr->nextPtr;
+        }
+        if(currentPtr != NULL)
+        {
+            tempPtr = currentPtr;
+            prevPtr->nextPtr = currentPtr->nextPtr;
+            free(tempPtr);
+            return value;
+        }
+    }
+
+    return -1;
+}
+void update(ListNodePtr* head, int value, int updated_value)
+{
+    ListNodePtr currentPtr = *head;
+    
+    if(currentPtr->data == value)
+    {
+        
+    }
+}
+node_t findFirst(ListNodePtr* head, int value);
